d9a6491504ae209afb9925f8527dabd2d2e8462f
vaishnavi-gupta-au17 <60317880+oneture-tech@users.noreply.github.com>
2024-10-05 23:23:10 +0530

    assignment

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-const express = require('express');
+// const express = require("express");
+// const app = express();
+// const port = 3000;
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// // app.use("/api/createTasks", require("./routes/tasks"));
+// app.listen(port, (err) => {
+//   if (err) {
+//     return console.log("Something bad happened", err);
+//   }
+//   console.log(`Server is listening on ${port}`);
+// });
+
+// module.exports = app;
+
+const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3300;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.listen(port, (err) => {
-    if (err) {
-        return console.log('Something bad happened', err);
-    }
-    console.log(`Server is listening on ${port}`);
+app.use(express.json);
+app.use(express.urlencoded({ extended: false }));
+app.listen(port, () => {
+  console.log("Server is running on 3300");
 });
-
-
 
 module.exports = app;
