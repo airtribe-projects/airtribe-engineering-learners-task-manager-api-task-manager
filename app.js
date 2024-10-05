51a9e78738cb29e8d7bebfba75d9978066933bab
Ankit Singh <rajputsonankit1999@gmail.com>
2024-10-05 22:00:44 +0530

    chnages related to task crud

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const taskRoutes = require('./routes/taskRoutes');
 const app = express();
-const port = 3000;
+const port = 4500;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Use the task routes
+app.use('/api/tasks', taskRoutes);
 
 app.listen(port, (err) => {
     if (err) {
